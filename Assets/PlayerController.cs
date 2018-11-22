87db64beafed2bd1efdea39c248b383eac52be82
eliasker <eliasker@gmail.com>
2018-11-22 09:39:16 +0200

    Update PlayerController.cs

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -25,7 +25,7 @@ public class PlayerController : MonoBehaviour
     // Pelin alussa 2 hp testailua varten
     private void Start()
     {
-        hitpoints = 2;
+        hitpoints = 1;
         forceField = false;
     }
 
