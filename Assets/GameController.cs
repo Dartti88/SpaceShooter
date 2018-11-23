1d9be3c05bf823e4a80120fced82597fe0116f2c
Dartti88 <dartti88@hotmail.com>
2018-11-23 11:30:19 +0200

    [Dart] Ultimate random map-lane-system

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -3,13 +3,13 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 using UnityEngine.UI;
 
+
 public class GameController : MonoBehaviour
 {
-    public GameObject hazard;
+    public GameObject asteroid;
     public GameObject pickup;   // Pickup
     public Vector3 spawnValues; // Spawnien koordinaattien raja-arvot
     public int hazardCount;
-    public int waveCount;   // Monesko taso on käynnissä
     public float spawnWait;
     public float startWait;
     public float waveWait;
@@ -23,6 +23,27 @@ public class GameController : MonoBehaviour
     private bool gameOver;
     private bool restart;
 
+    //Wave counters
+    
+    public int waveCount;   // Monesko taso on käynnissä
+    /*
+    private SpaceController currentSpace;
+    private GameObject[,] currentMap;
+    private GameObject[] currentWave;
+    private GameObject currentWaveHazard;//Mikä hazardi on vuorossa
+    */
+
+    private int _diff;
+    private int width = 3;
+    private int height = 10;
+    private int hazard_number = 10;
+
+    public GameObject[,][] map;
+    public GameObject[] list;
+    private GameObject[] currentHazardList;
+    System.Random rnd = new System.Random();
+
+
     void Start()
     {
         gameOver = false;
@@ -31,10 +52,35 @@ public class GameController : MonoBehaviour
         gameOverText.text = "";
 
         score = 0;
-        waveCount = 1;          // Wave count
+        waveCount = 0;          // Wave count
         waveText.text = "";     // UI:hin tulostuva teksti
         UpdateScore();
         StartCoroutine(SpawnWaves());
+
+        //Get Map
+
+        //currentSpace = GameObject.FindObjectOfType<SpaceController>();
+        //currentMap = currentSpace.getMap();
+        //currentMap= SpaceController.getMap(); //Errori Visual Studiossa ei haittaa. Tomii Unityssä normaalisti
+
+
+        ////*** MAP
+        //Create map and fill it with list of random hazards
+        map = new GameObject[width, height][];
+        
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                //Create random hazards by difficulty
+                //GameObject[] lista = { pickup, pickup, asteroid, pickup, asteroid, pickup, pickup, asteroid, pickup, asteroid, pickup, pickup, asteroid, pickup, asteroid };
+                //map[i, j] = lista;
+                map[j,i]=WaveList(i);
+            }
+        }
+    
+        //map[0, 0] = new GameObject[] { pickup, pickup, pickup, pickup, asteroid, pickup, pickup, asteroid, pickup, asteroid, pickup, pickup, asteroid, pickup, asteroid };
+        //map[0, 1] = new GameObject[] { pickup, pickup, asteroid, pickup, asteroid, pickup, pickup, asteroid, pickup, asteroid, pickup, pickup, asteroid, pickup, asteroid };
     }
 
     void Update()
@@ -46,6 +92,37 @@ public class GameController : MonoBehaviour
                 Application.LoadLevel(Application.loadedLevel);
             }
         }
+    }
+
+    public GameObject[] WaveList(int diff)
+    {
+        list = new GameObject[hazard_number + 1];
+        _diff = diff;
+
+        for (int i = 0; i < hazard_number; i++)
+        {
+            
+            int enemy = rnd.Next(1, 3);
+            //add hazard to list
+
+            switch (enemy)
+            {
+                case 1: list[i] = pickup; break;
+                case 2: list[i] = asteroid; break;
+            }
+            /*
+            if (i % 2 == 0)
+            {
+                list[i] = pickup;
+            }
+            else
+            {
+                list[i] = asteroid;
+            }
+            */
+        }
+
+        return list;
     }
 
     IEnumerator SpawnWaves()
@@ -64,7 +141,8 @@ public class GameController : MonoBehaviour
             {
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
-                Instantiate(hazard, spawnPosition, spawnRotation);
+                currentHazardList = map[1, waveCount];
+                Instantiate(currentHazardList[i], spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait);
             }
             yield return new WaitForSeconds(waveWait);
@@ -73,7 +151,8 @@ public class GameController : MonoBehaviour
             // Spawnataan pickup jokaisen aallon jälkeen
             Vector3 pickUpSpawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
             Quaternion pickUpSpawnRotation = Quaternion.identity;
-            Instantiate(pickup, pickUpSpawnPosition, pickUpSpawnRotation);
+
+            //Instantiate(pickup, pickUpSpawnPosition, pickUpSpawnRotation);
 
             if (gameOver == true)
             {
